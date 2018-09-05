4a25c1429b6d88d69891ec02032e34b50db019a2
luan.peixoto <luan.peixoto@aevo.com.br>
2018-09-05 11:16:17 -0300

    Start service working, but only at adm mode

--- ServicesPainelController/Form1.Designer.cs
+++ ServicesPainelController/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows services";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(188, 134);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(67, 297);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(222, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(337, 334);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- ServicesPainelController/Form1.cs
+++ ServicesPainelController/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,87 @@ namespace ServicesPainelController
         public Form1()
         {
             InitializeComponent();
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.Visible = false;
+            GetwindowServices();
+
+
+        }
+
+        private void GetwindowServices()
+        {
+            //throw new NotImplementedException();
+            ServiceController[] service;
+            service = ServiceController.GetServices();
+            comboBox1.Items.Clear();
+            for (int i = 0; i < service.Length; i++)
+            {
+                comboBox1.Items.Add(service[i].ServiceName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var singleService = new ServiceController((string)e.Argument);
+            if (singleService.Status.Equals(ServiceControllerStatus.Stopped) || (singleService.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+
+                singleService.Start();
+            }
+            else
+            {
+                singleService.Stop();
+            }
+            
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //this.button1.Enabled = false;
+            this.progressBar1.Visible = false;
+            if (e.Error!=null)
+            {
+                MessageBox.Show(e.Error.ToString(),"Could not start the service");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = true;
+            this.progressBar1.Visible = true;
+            this.backgroundWorker1.RunWorkerAsync(comboBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = true;
+            this.progressBar1.Visible = true;
+            this.backgroundWorker1.RunWorkerAsync(comboBox1.Text);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ServiceController singleService = new ServiceController(comboBox1.Text);
+            if (singleService.Status.Equals(ServiceControllerStatus.Stopped) || (singleService.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+
         }
     }
 }
